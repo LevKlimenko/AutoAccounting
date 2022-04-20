74b230463ac7a570b2da0c30aadf3dc5f19a08a9
LevKlimenko <102366057+LevKlimenko@users.noreply.github.com>
2022-04-20 11:07:47 +0300

    Delete YearlyReport.java

--- src/YearlyReport.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class YearlyReport{
-    GetYearlyReport yearlyReport = new GetYearlyReport();
-
-    public static void yearReport(){
-        //if (hashMapYearReportRashod.();
-    }
-}
